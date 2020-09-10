ae6d0c3701d09fbe20e4471380cbb3775e2f6feb
LEARN Academy <devjoelmichael@protonmail.com>
2020-09-09 17:40:33 -0700

    Just need some CSS

--- src/App.js
+++ src/App.js
@@ -13,10 +13,12 @@ class App extends React.Component {
 
   getAnswer = () => {
     // destructures answerArray out of state
-    const { answerArray } = this.state
+    const { answerArray } = this.state 
     // write your code here!
+    var quotes = ["Say it ain't so. I will not go. Turn the lights off. Carry me home.", "You have chosen the path of the black sheep rather than that of the unicorns and puppies.", "Fate fell short this time.", "The perfect life is waiting for you.", "It takes disaster to learn a lesson.", "Rise above! You're gonna rise above!", "They say what doesn't kill us, makes us who we are", "Hey now, you're an all star get your game on, go play hey now, you're a rock star get the show on, get paid and all that glitters is gold only shooting stars break the mold.", "Dan Bakitus says you're shallow he said you've got no integrity, but you don't know Dan Bakitus and Dan Bakitus, he don't know you!", "Oh yeah, you've got style it's plain to see!"]
+    let randomNumber = Math.floor(Math.random() * (quotes.length));
     // Update the return statement below to reflect the outcome of your code.
-    return this.state.question
+    return quotes[randomNumber];
   }
 
   handleChange = (e) => {
